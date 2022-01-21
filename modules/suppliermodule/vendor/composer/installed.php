c84f6b79862c6db816a0e138c304ec2ea36e7bad
Arthur JORIS <ar.joris@laposte.net>
2022-01-21 11:47:08 +0100

    Add module

---
+++ modules/suppliermodule/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fb9fd95f47865c584660621f144bff7f9cd3f7e4',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fb9fd95f47865c584660621f144bff7f9cd3f7e4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
